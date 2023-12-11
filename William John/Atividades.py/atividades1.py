e4139527e5f96ce42a9233a2a70b6f87b194d1c8
William <130475382+09190433130@users.noreply.github.com>
2023-12-11 17:29:29 -0300

    Atualizando o Python

---
+++ William John/Atividades.py/atividades1.py
@@ -0,0 +1,7 @@
+vetor = []
+for i in range(5):
+    numero = int(input(f"Digite o {i+1}° número inteiro:"))
+    vetor.append(numero)
+print("Numero digitados:")
+for num in vetor:
+    print(num)
---
+++ William John/Atividades.py/atividades2.py
@@ -0,0 +1,6 @@
+limite = int(input("Digite um número: "))
+if limite > 0:
+    for numero in range(limite):
+        print(numero)
+    else:
+        print("Por favor, digite um número positivo.")
