c240fbb82f399c23429f50ecd952de10a9fac15d
William <130475382+09190433130@users.noreply.github.com>
2023-12-13 17:28:09 -0300

    atutalizando atividades Python

---
+++ William John/Atividades.py/atividades3.py
@@ -0,0 +1,5 @@
+for num in range(0, 11):
+    if num % 10 == 0:
+        print("Encontrou um número válido ", num)
+        continue
+    print("Encontrou um número inválido ", num)
---
+++ William John/Atividades.py/linhasdecontinuação.py
@@ -0,0 +1,3 @@
+existe_10_tipos_de_pessoas = True
+if existe_10_tipos_de_pessoas:
+    print("As que entendem e não entendem binário.")
